dccdbc6f5720b35c32abe98a76e1f09b6c95696e
林尧 <1542319159@qq.com>
2016-01-26 09:24:50 +0800

    j

--- sample/src/main/java/org/xutils/sample/BaseActivity.java
+++ sample/src/main/java/org/xutils/sample/BaseActivity.java
@@ -10,6 +10,7 @@ import org.xutils.x;
  */
 public class BaseActivity extends AppCompatActivity {
 
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
